47ba379091e2032f55c494e73ddb416e0250557e
andrecosta14 <ac.14@live.com.pt>
2024-02-11 13:36:41 +0000

    ...

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -142,7 +142,7 @@ router.put('/edit-post/:id', authMiddleware, async (req, res) => {
             updatedAt: Date.now()
         });
 
-        res.redirect(`/edit-post/${req.params.id}`);
+        res.redirect('/dashboard');
 
     }   catch (error) {
         console.log(error);
